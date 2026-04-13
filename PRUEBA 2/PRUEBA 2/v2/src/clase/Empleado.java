473549675e692c1ab81289e5b4fd7209fb12fd0d
Pelaez12 <fabrizziopelaez91@gmail.com>
2026-04-12 20:45:34 -0500

    AÑADI EL APARTADO VISUAL ADEMAS DE CREAR EL BOTON AGREGAR

---
+++ PRUEBA 2/PRUEBA 2/v2/src/clase/Empleado.java
@@ -0,0 +1,48 @@
+package clase;
+
+public class Empleado {
+	private String nom;
+	private String ape;
+	private int num;
+	private int dni;
+	private String correo;
+	public Empleado(String nom, String ape, int num, int dni, String correo) {
+		super();
+		this.nom = nom;
+		this.ape = ape;
+		this.num = num;
+		this.dni = dni;
+		this.correo = correo;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getApe() {
+		return ape;
+	}
+	public void setApe(String ape) {
+		this.ape = ape;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public int getDni() {
+		return dni;
+	}
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	
+}
